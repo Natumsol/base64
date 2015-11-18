1ede4242d4a591b5a37378b23a2972af9a2af68e
natumsol <natumsol@gmail.com>
2015-11-18 22:14:28 +0800

    finish image to base64

--- js/base64.js
+++ js/base64.js
@@ -1,11 +1,14 @@
 define(function(require, exports, module) {
-	/**
-	 * @param  {[type]}
-	 * @return {[type]}
-	 */
-	var code = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".split("");
 
-	function strToBase64(bitString) {
+	var code = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".split(""); //索引表
+
+	/**
+	 * @author natumsol@gmail.com
+	 * @description 将二进制序列转换为Base64编码
+	 * @param  {String}
+	 * @return {String}
+	 */
+	function binToBase64(bitString) {
 		var result = "";
 		var tail = bitString.length % 6;
 		var bitStringTemp1 = bitString.substr(0, bitString.length - tail);
@@ -22,7 +25,13 @@ define(function(require, exports, module) {
 		return result;
 	}
 
-	function base64ToStr(str) {
+	/**
+	 * @author natumsol@gmail.com
+	 * @description 将base64编码转换为二进制序列
+	 * @param  {String}
+	 * @return {String}
+	 */
+	function base64ToBin(str) {
 		var bitString = "";
 		var tail = 0;
 		for (var i = 0; i < str.length; i++) {
@@ -33,11 +42,13 @@ define(function(require, exports, module) {
 				tail++;
 			}
 		}
-		return bitString.substr(0, bitString.length - (6 - tail * 2));
+		return bitString.substr(0, bitString.length - tail * 2);
 	}
+
 	/**
-	 * @param  {[type]}
-	 * @return {[type]}
+	 * @description 将字符转换为二进制序列
+	 * @param  {String} str 
+	 * @return {String}    
 	 */
 	function stringToBin(str) {
 		var result = "";
@@ -47,7 +58,10 @@ define(function(require, exports, module) {
 		}
 		return result;
 	}
-
+	/**
+	 * @description 将二进制序列转换为字符串
+	 * @param {String} Bin 
+	 */
 	function BinToStr(Bin) {
 		var result = "";
 		for (var i = 0; i < Bin.length; i += 8) {
@@ -55,10 +69,21 @@ define(function(require, exports, module) {
 		}
 		return result;
 	}
+
+	function numToString(num) {
+		var bin = num.toString(2);
+		return new Array(9 - bin.length).join(0) + bin;
+	}
+
 	exports.base64 = function(str) {
-		return strToBase64(stringToBin(str));
+		return binToBase64(stringToBin(str));
 	}
 	exports.decodeBase64 = function(str) {
-		return BinToStr(base64ToStr(str));
+		return BinToStr(base64ToBin(str));
 	}
+	exports.numToString = numToString;
+	exports.stringToBin = stringToBin;
+	exports.binToBase64 = binToBase64;
+	exports.base64ToBin = base64ToBin;
+	exports.BinToStr = BinToStr;
 })
--- js/index.js
+++ js/index.js
@@ -1,3 +1,63 @@
-require(["./base64"], function(base){
-	base.init();
+require(["./js/base64"], function(base) {
+	var $ = document.getElementById.bind(document);
+	$("convert1").addEventListener("click", function() {
+		$("target1").value = base.base64($("source1").value);
+	}, false);
+	$("convert2").addEventListener("click", function() {
+		$("target2").value = base.decodeBase64($("source2").value);
+	}, false);
+
+	function getCanvas(w, h) {
+		var c = document.createElement('canvas');
+		c.width = w;
+		c.height = h;
+		return c;
+	}
+
+	function getPixels(img) {
+		var c = getCanvas(img.width, img.height);
+		var ctx = c.getContext('2d');
+		ctx.drawImage(img, 0, 0);
+		return ctx.getImageData(0, 0, c.width, c.height);
+	}
+
+
+	function img2Base64(img) {
+		var imgData = getPixels(img).data;
+		var imgWidth = getPixels(img).width;
+		var imgHeight = getPixels(img).height;
+		var bin = "";
+		for (var i = 0; i < imgData.length; i++) {
+			bin += base.numToString(imgData[i]);
+		}
+		bin = bin + base.stringToBin("$$" + imgWidth + "," + imgHeight + "$$");
+		return base.binToBase64(bin);
+	}
+
+
+
+	function paint(imgData) {
+		var canvas = document.getElementById("myCanvas");
+		var ctx = canvas.getContext("2d");
+		ctx.fillRect(0, 0, imgData.width, imgData.height);
+		ctx.putImageData(imgData, 0, 0);
+	}
+
+	function base642img(data) {
+		var str = base.BinToStr(base.base64ToBin(data));
+		var imgWidth = str.match(/\$\$(\d+),(\d+)\$\$$/, "")[1];
+		var imgHeight = str.match(/\$\$(\d+),(\d+)\$\$$/, "")[2]
+		var imgData = base.base64ToBin(data).replace(base.stringToBin("$$" + imgWidth + "," + imgHeight + "$$"), "");
+
+		var ImageDataArray = new Uint8ClampedArray(imgWidth * imgHeight * 4);
+		for (var i = 0; i < ImageDataArray.length; i++) {
+			ImageDataArray[i] = parseInt(imgData.substr(i * 8, 8), 2);
+		}
+		return new ImageData(ImageDataArray, imgWidth, imgHeight);
+
+	}
+
+	img = $("img");
+	$("imgData").value = img2Base64(img).substr(0,2000) + "\n太多了后面省略。。";
+	paint(base642img(img2Base64(img)));
 })
